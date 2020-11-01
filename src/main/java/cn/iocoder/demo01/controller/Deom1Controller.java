de58a9f1a03c5a691c66055be008e36816a749f2
wany82075 <wany82075@163.com>
2020-11-01 16:29:28 +0800

    测试提交

---
+++ src/main/java/cn/iocoder/demo01/controller/Deom1Controller.java
@@ -0,0 +1,10 @@
+package cn.iocoder.demo01.controller;
+
+/**
+ * @ClassName : Deom1Controller
+ * @Description :
+ * @Author : wangyan
+ * @Date: 2020-11-01 15:58
+ */
+public class Deom1Controller {
+}
